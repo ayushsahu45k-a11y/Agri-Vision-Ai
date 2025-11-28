3e526e98074de889948656577476c8ca5cce1e2b
Ayush Sahu <ayushsahu.45k@gmail.com>
2025-11-28 13:25:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-
-  return {
-    base: "./",                    // 🔥 Required to fix 404 on Vercel
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.API_KEY) // 🔥 Safe env usage
-    },
-    build: {
-      outDir: "dist",
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
 });
+
